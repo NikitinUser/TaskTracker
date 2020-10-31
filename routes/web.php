a53915945cc277bfb7f7e35c7455758f1fbfbae8
root <root@debian.Debian>
2020-10-31 23:09:17 +0500

    edit start page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("https://nikitinuser.github.io/ToDo/");	
 });
 
 Auth::routes();
