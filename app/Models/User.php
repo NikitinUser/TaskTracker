5c66229e042ff0d4645211cf26fa89f313ab0396
NikitinUser <jo.kent@mail.ru>
2021-10-17 10:20:27 +0500

    Add roles provider

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use \Auth;
+use Illuminate\Support\Facades\DB;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    protected $arrRolesDescription = [
+        "admin"  => 1,
+        "user" => 6 
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +49,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function hasRole($role)
+    {
+        if (!auth()->check()) {
+            return false;
+        }
+
+        if (!isset($this->arrRolesDescription[$role]) ) {
+            return false;
+        }
+
+        $needRole = $this->arrRolesDescription[$role];
+        $userid = intval(auth()->user()->id);
+        $userRoles = DB::table('roles')
+                    ->select('roles.id')
+                    ->join('roles_for_user', 'roles.id', '=', 'roles_for_user.id_role')
+                    ->where('roles_for_user.id_user', '=', $userid)
+                    ->get()
+                    ->toArray();
+        
+        $roles = [];
+
+        foreach ($userRoles as $key => $value) {
+            array_push($roles, $value->id);
+        }
+        //dd($roles);
+
+        if ( in_array($needRole, $roles) ) {
+            return true;
+        }
+
+        return false;
+    }
 }
