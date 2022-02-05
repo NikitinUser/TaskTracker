1cf5c79b5aaec12f5a8ff28d03500d5ba5b28467
NikitinUser <jo.kent@mail.ru>
2022-02-06 04:57:51 +0500

    Move validation to FormRequest

--- app/Http/Requests/SwapTheTypeOfTaskRequest.php
+++ app/Http/Requests/SwapTheTypeOfTaskRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class SwapTheTypeOfTaskRequest extends FormRequest
 {
@@ -25,9 +27,37 @@ class SwapTheTypeOfTaskRequest extends FormRequest
     public function rules()
     {
         return [
-            'id'    => 'required',
-            'date'  => 'required', 
-            'type'  => 'required|integer', 
+            'id'    => 'required|integer',
+            'date'  => 'required|date|max:18', 
+            'type'  => 'required|integer|min:0|max:3',
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        $messages = [
+            'date.required' => 'Двта создания задачи должна быть сформирована',
+            'date.date' => 'Дата задачи должна быть датой',
+            'date.max:18' => 'Максимальное длина даты 18 символов',
+
+            'type.required' => 'Необходимо выбрать тип задачи',
+            'type.integer' => 'Тип задачи должен быть целым числом',
+            'type.min:0' => 'Тип задачи начинает от 0',
+            'type.max:900' => 'Максимальное значение для типа задачи - 3',
+
+            'id.required' => 'Идентификатор задачи должен быть указан',
+            'id.integer' => 'Идентификатор задачи должен быть целым числом',
+        ];
+        return $messages;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors(),'status' => true], 422));
     }
 }
