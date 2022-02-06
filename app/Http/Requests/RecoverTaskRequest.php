34a8ace6839579a86e82ee00996f91db9ac930b3
NikitinUser <jo.kent@mail.ru>
2022-02-06 05:00:55 +0500

    Move validation to FormRequest

--- app/Http/Requests/RecoverTaskRequest.php
+++ app/Http/Requests/RecoverTaskRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class RecoverTaskRequest extends FormRequest
 {
@@ -27,5 +29,24 @@ class RecoverTaskRequest extends FormRequest
         return [
             'id'    => 'required|integer',
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        $messages = [
+            'id.required' => 'Идентификатор задачи должен быть указан',
+            'id.integer' => 'Идентификатор задачи должен быть целым числом',
+        ];
+        return $messages;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(['errors' => $validator->errors(),'status' => true], 422));
     }
 }
