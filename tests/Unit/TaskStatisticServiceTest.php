b7cae950d51be2d4d1d26ac9d41bd094d1d28590
NikitinUser <jo.kent@mail.ru>
2022-06-27 00:06:32 +0500

    update test

---
+++ tests/Unit/TaskStatisticServiceTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Services\TaskStatisticService;
+use App\Models\TasksMain;
+use App\Models\TaskStatistic;
+use App\Models\User;
+
+class TaskStatisticServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testGetCountTasks()
+    {
+        $statisticService = new TaskStatisticService();
+        $userId = $this->createNewUserGetId();
+        
+        $this->authByUserId($userId);
+
+        $res = $statisticService->getCountTasks($userId);
+        
+        $this->assertIsArray($res);
+    }
+
+    /**
+     * @test
+     */
+    public function testCommitDoneTaskByExistUserid()
+    {
+        $statisticService = new TaskStatisticService();
+        $userId = $this->createNewUserGetId();
+
+        $this->authByUserId($userId);
+
+        $task = TasksMain::create(
+            [
+                "task" => "3232",
+                "userid" => $userId,
+                "dt_task" => "2022-06-26 03:14:23",
+                "type" => 0,
+                "priority" => 0,
+            ]
+        );
+
+        $task->type = TasksMain::TYPE_DONE_TASK;
+
+        $res = true;
+        try {
+            $statisticService->commitDoneTaskByUserid($task);
+        } catch (\Exception $e) {
+            $res = false;
+        }
+
+        $this->assertTrue($res);
+    }
+
+    /**
+     * @test
+     */
+    public function testCommitDoneTaskByNotExistUserid()
+    {
+        $statisticService = new TaskStatisticService();
+        $userId = 0;
+
+        $this->authByUserId($userId);
+
+        $task = new TasksMain();
+
+        $task->type = TasksMain::TYPE_DONE_TASK;
+
+        $res = true;
+        try {
+            $statisticService->commitDoneTaskByUserid($task);
+        } catch (\Exception $e) {
+            $res = false;
+        }
+
+        $this->assertFalse($res);
+    }
+
+    /**
+     * @test
+     */
+    public function testAddStatisticToUser()
+    {
+        $statisticService = new TaskStatisticService();
+
+        $userId = $this->createNewUserGetId();
+
+        $res = true;
+        try {
+            $statisticService->addStatisticToUser($userId);
+        } catch (\Exception $e) {
+            $res = false;
+        }
+        
+        $this->assertTrue($res);
+    }
+
+    private function authByUserId(int $userId)
+    {
+        $user = User::where("id", $userId)->first();
+        \Auth::shouldReceive('guard')->andReturnSelf()
+                ->shouldReceive('user')->andReturn($user)
+                ->shouldReceive('check')->andReturn(true);
+    }
+
+    private function createNewUserGetId()
+    {
+        return User::insertGetId(
+            [
+                "login" => "login" . date("U") . rand(1, 1990009),
+                "password" => "123",
+                "block" => 0,
+            ]
+        );
+    }
+}
