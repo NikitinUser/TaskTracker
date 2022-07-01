1be830ddbd2c5939fe31ae50598ace3c06179a63
NikitinUser <jo.kent@mail.ru>
2022-07-02 01:11:00 +0500

    add test for changing task

---
+++ tests/Feature/ChangeTaskTest.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tests\TestsHelper;
+use App\Models\User;
+use App\Models\TasksMain;
+
+class ChangeTaskTest extends TestCase
+{
+    use TestsHelper;
+
+    /**
+     * @test
+     */
+    public function testChangeTask()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => "task_text",
+            'theme' => "Testing",
+            'priorityTask' => 1,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskWithoutTask()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'theme' => "Testing",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskInvalidMinLengthTask()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => $this->generateStringLessMin(),
+            'theme' => "Testing",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskInvalidMaxLengthTask()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => $this->generateStringMoreMax(),
+            'theme' => "Testing",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskWithoutTheme()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => "text",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskWithInvalidTheme()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => "text",
+            'theme' => "!@#$#%^%&(**)_+=?><<M'`~",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskWithoutPriority()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'id' => $task->id,
+            'task' => "text",
+            'theme' => "Testing",
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function testChangeTaskWithoutId()
+    {
+        $userId = $this->createNewUserGetId();
+        $user = User::where("id", $userId)->first();
+
+        $task = TasksMain::first();
+
+        $request = [
+            'task' => "text",
+            'theme' => "Testing",
+            'priorityTask' => 0,
+        ];
+
+        $response = $this->actingAs($user)
+            ->post('/changeTask', $request);
+ 
+        $response->assertStatus(422);
+    }
+}
