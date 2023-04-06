88e9f61a58a07705664de7a27faaaeba2e99ef60
NikitinUser <admin@MacBook-Air-admin.local>
2023-04-06 23:21:13 +0600

    change task item design

--- src/resources/vue_front/src/components/TaskItem.vue
+++ src/resources/vue_front/src/components/TaskItem.vue
@@ -1,98 +1,87 @@
 <template>
-    <div v-bind:id="id" style="min-height: 200px;">
+    <div v-bind:id="id">
         <div>
-            <div class="d-flex flex-row mb-3" style="min-height: 150px;">
-                <div class="text-white d-flex flex-column" style="max-width: 20%">
-                    <div class="mb-3 d-flex flex-column">
-                        <em style="font-size: small">
-                            {{ date.split(" ")[0] }}
-                        </em>
-                        <em style="font-size: small">
-                            {{ date.split(" ")[1] }}
-                        </em>
-                    </div>
+            <div class="text-white mb-3">
+                <em style="font-size: small">{{ date }}</em>
 
-                    <div>
-                        <button type="button" class="btn btn-outline-light btn-sm"
-                            v-on:click="visibleTask=!visibleTask">
-                            {{visibleTask?'Скрыть':'Показать'}}
-                        </button>
-                    </div>
+                <button type="button" class="btn btn-outline-light ms-2"
+                    v-on:click="visibleTask=!visibleTask">
+                    <i v-if="visibleTask" class="fa fa-eye-slash" aria-hidden="true"></i>
+                    <i v-else class="fa fa-eye" aria-hidden="true"></i>
+                </button>
+
+                <div class="text-white d-flex flex-row pull-right"
+                    v-if="currentRoute == '/demo'">
+                    <TaskActionButton
+                        buttonIcon = "fa fa-check-square"
+                        buttonClass = "pull-right btn btn-outline-success w-100"
+                        action = "removeTaskFromLocal"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
                 </div>
+                <div class="text-white d-flex flex-row pull-right"
+                    v-else>
+                    <TaskActionButton 
+                        v-if="currentRoute != '/done'"
+                        buttonIcon = "fa fa-check-square"
+                        buttonClass = "pull-right btn btn-outline-success w-100"
+                        action = "swapTaskToDone"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
 
-                <div class="text-white d-flex justify-content-center w-100 ms-3 me-1">
-                    <span v-bind:id="id" v-show="visibleTask">
-                        {{ task }}
-                    </span>
+                    <TaskActionButton
+                        v-else
+                        buttonIcon = "fa fa-trash"
+                        buttonClass = "pull-right btn btn-outline-danger w-100"
+                        action = "deleteTask"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
+
+                    <TaskActionButton
+                        buttonIcon = "fa fa-location-arrow"
+                        buttonClass = "pull-right btn btn-outline-success w-100"
+                        action = "swapTaskToTasks"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
+
+                    <TaskActionButton
+                        buttonIcon = "fa fa-bookmark"
+                        buttonClass = "pull-right btn btn-outline-primary w-100"
+                        action = "swapTaskToBookmarks"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
+
+                    <TaskActionButton
+                        buttonIcon = "fa fa-archive"
+                        buttonClass = "pull-right btn btn-outline-info w-100"
+                        action = "swapTaskToArchive"
+                        :idTask="id"
+                        :changedTask="task"
+                        :type="type"
+                    ></TaskActionButton>
+
+                    <TaskActionButton
+                        buttonIcon = "fa fa-pencil-square"
+                        buttonClass = "pull-right btn btn-outline-warning w-100"
+                        action = "editTask"
+                        :idTask="id"
+                    ></TaskActionButton>
                 </div>
             </div>
 
-            <div class="text-white d-flex flex-row pull-right"
-                v-if="currentRoute == '/demo'">
-                <TaskActionButton
-                    buttonIcon = "fa fa-check-square"
-                    buttonClass = "pull-right btn btn-outline-success w-100"
-                    action = "removeTaskFromLocal"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-            </div>
-            <div class="text-white d-flex flex-row pull-right"
-                v-else>
-                <TaskActionButton 
-                    v-if="currentRoute != '/done'"
-                    buttonIcon = "fa fa-check-square"
-                    buttonClass = "pull-right btn btn-outline-success w-100"
-                    action = "swapTaskToDone"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-
-                <TaskActionButton
-                    v-else
-                    buttonIcon = "fa fa-trash"
-                    buttonClass = "pull-right btn btn-outline-danger w-100"
-                    action = "deleteTask"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-
-                <TaskActionButton
-                    buttonIcon = "fa fa-location-arrow"
-                    buttonClass = "pull-right btn btn-outline-success w-100"
-                    action = "swapTaskToTasks"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-
-                <TaskActionButton
-                    buttonIcon = "fa fa-bookmark"
-                    buttonClass = "pull-right btn btn-outline-primary w-100"
-                    action = "swapTaskToBookmarks"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-
-                <TaskActionButton
-                    buttonIcon = "fa fa-archive"
-                    buttonClass = "pull-right btn btn-outline-info w-100"
-                    action = "swapTaskToArchive"
-                    :idTask="id"
-                    :changedTask="task"
-                    :type="type"
-                ></TaskActionButton>
-
-                <TaskActionButton
-                    buttonIcon = "fa fa-pencil-square"
-                    buttonClass = "pull-right btn btn-outline-warning w-100"
-                    action = "editTask"
-                    :idTask="id"
-                ></TaskActionButton>
+            <div class="text-white mb-2">
+                <textarea v-bind:id="id" v-show="visibleTask" class="form-control"
+                    :value="task" readonly></textarea>
             </div>
         </div>
 
